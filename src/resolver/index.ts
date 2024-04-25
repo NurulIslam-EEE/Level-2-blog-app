a5c06b066b89ba8a4b7e68cf9c6bbd4dbccb47a2
NurulIslam-EEE <nurul.islam02310@gmail.com>
2024-04-25 22:56:53 +0600

    module 68-8 done

--- src/resolver/index.ts
+++ src/resolver/index.ts
@@ -37,5 +37,35 @@ export const resolvers = {
         token,
       };
     },
+    signin: async (parent: any, args: any, context: any) => {
+      const user = await prisma.user.findFirst({
+        where: {
+          email: args.email,
+        },
+      });
+      // console.log("uuuu", user);
+      if (!user) {
+        return {
+          userError: "User not found!",
+          token: null,
+        };
+      }
+
+      const correctPass = await bcrypt.compare(args.password, user.password);
+
+      if (!correctPass) {
+        return {
+          userError: "Incorrect Password!",
+          token: null,
+        };
+      }
+      const token = await jwt.sign({ userId: user.id }, "signature", {
+        expiresIn: "1d",
+      });
+      return {
+        userError: null,
+        token,
+      };
+    },
   },
 };
