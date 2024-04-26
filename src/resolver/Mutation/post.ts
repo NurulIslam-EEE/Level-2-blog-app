613d4682ba546ef641a44e41ee9a01132cb0142c
NurulIslam-EEE <nurul.islam02310@gmail.com>
2024-04-27 03:32:21 +0600

    module-69-5 done

--- src/resolver/Mutation/post.ts
+++ src/resolver/Mutation/post.ts
@@ -30,6 +30,58 @@ export const postResolvers = {
     };
   },
   updatePost: async (parent: any, args: any, { prisma, userInfo }: any) => {
-    console.log(args, userInfo);
+    // console.log(args, userInfo);
+    if (!userInfo) {
+      return {
+        userError: "Unauthorized",
+        post: null,
+      };
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userInfo.userId,
+      },
+    });
+
+    if (!user) {
+      return {
+        userError: "user not found",
+        post: null,
+      };
+    }
+
+    const post = await prisma.post.findUnique({
+      where: {
+        id: Number(args.postId),
+      },
+    });
+
+    if (!post) {
+      return {
+        userError: "post not found",
+        post: null,
+      };
+    }
+
+    if (post.authorId !== user.id) {
+      return {
+        userError: "post not own by you",
+        post: null,
+      };
+    }
+
+    console.log("user", user, "ppp", post);
+    const updatedPost = await prisma.post.update({
+      where: {
+        id: Number(args.postId),
+      },
+      data: args.post,
+    });
+
+    return {
+      userError: null,
+      post: updatedPost,
+    };
   },
 };
