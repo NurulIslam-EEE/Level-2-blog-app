5d487a1a215ecdc4da8ca2f59a7af558e917ce2b
NurulIslam-EEE <nurul.islam02310@gmail.com>
2024-04-28 05:50:11 +0600

    module-69-7 done

--- src/resolver/Query/Query.ts
+++ src/resolver/Query/Query.ts
@@ -1,10 +1,35 @@
 export const Query = {
+  me: async (parent: any, args: any, { prisma, userInfo }: any) => {
+    console.log(userInfo, "cccc");
+    return await prisma.user.findUnique({
+      where: {
+        id: userInfo.userId,
+      },
+    });
+  },
+  profile: async (parent: any, args: any, { prisma, userInfo }: any) => {
+    console.log(args, "cccc");
+    return await prisma.profile.findUnique({
+      where: {
+        userId: Number(userInfo.userId),
+      },
+    });
+  },
   users: async (parent: any, args: any, { prisma }: any) => {
     console.log(await prisma.user.findMany());
     return await prisma.user.findMany();
   },
   posts: async (parent: any, args: any, { prisma }: any) => {
     console.log(await prisma.post.findMany());
-    return await prisma.post.findMany();
+    return await prisma.post.findMany({
+      where: {
+        published: true,
+      },
+      orderBy: [
+        {
+          createdAt: "asc",
+        },
+      ],
+    });
   },
 };
